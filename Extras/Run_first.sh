d67a8809df7afbf15c71137e67e00b8dce58a949
newb7171 <rotheory13@gmail.com>
2026-06-07 20:51:24 +0800

    Change apt to apt-get in Run_first.sh

--- Extras/Run_first.sh
+++ Extras/Run_first.sh
@@ -2,6 +2,6 @@
 
 sed -i '/^Types:/ {/deb-src/! s/$/ deb-src/;}' /etc/apt/sources.list.d/ubuntu.sources
 
-apt update && apt upgrade -y && \
-apt build-dep mesa -y && \
-apt install -y cmake pkg-config patchelf wget
+apt-get update && apt upgrade -y && \
+apt-get build-dep mesa -y && \
+apt-get install -y cmake pkg-config patchelf wget

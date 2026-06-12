33f93c44ea209e8793c45992e78969c5e5f9b1a8
newb7171 <rotheory13@gmail.com>
2026-06-12 22:21:16 +0800

    Delete Extras directory

--- Extras/Run_first.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-sed -i '/^Types:/ {/deb-src/! s/$/ deb-src/;}' /etc/apt/sources.list.d/ubuntu.sources
-
-apt-get update && apt upgrade -y && \
-apt-get build-dep mesa -y && \
-apt-get install -y cmake pkg-config patchelf wget

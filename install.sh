0eaa2a72dc3706df452c34b167c96315974c6070
newb7171 <rotheory13@gmail.com>
2026-06-12 22:25:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-#
+#!/bin/bash -e
+
+sed -i '/^Types:/ s/deb/deb deb-src/' /etc/apt/sources.list.d/debian.sources
+
+apt update && apt upgrade -y && apt build-dep mesa -y && apt install wget patchelf zip -y

66ffc1a2bdca501c35b651fbfe61dbfa80e42905
newb7171 <rotheory13@gmail.com>
2026-06-20 08:10:39 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 workdir="$(pwd)/turnip_workdir"
 ndk="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/bin"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
-BUILD_VERSION="26.2.0-V4"
+BUILD_VERSION="26.2.0_V4"
 PATCH_1="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/patch.patch"
+PATCH_2="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/whitebelyash-patch-experimental.diff"
+
 
 echo "Only works in debian!!! press Ctrl + C to exit"
 echo "Installing build dependencies..."
@@ -36,7 +38,9 @@ cd mesa
 
 echo "Applying patch..."
 wget "$PATCH_1"
+wget "PATCH_2"
 git apply patch.patch
+patch -p1 whitebelyash-patch-experimental.diff
 
 echo "#define TUGEN8_DRV_VERSION \"$BUILD_VERSION\"" > ./src/freedreno/vulkan/tu_version.h
 

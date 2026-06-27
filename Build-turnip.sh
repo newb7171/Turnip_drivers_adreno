2095ce062ec2b2b55e41caae91f8fd6e30c666d2
JustCallMeJade <rotheory13@gmail.com>
2026-06-27 16:07:22 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -4,8 +4,8 @@ workdir="$(pwd)/turnip_workdir"
 ndk="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/bin" #yes r29 is the directory
 sysroot="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
-BUILD_VERSION="26.2.0-V5.0" # this script is always maintained just update when new version.
-VERSION="V5.0"
+BUILD_VERSION="26.2.0-V5.1" #this script is always maintained just update when new version.
+VERSION="V5.1"
 PATCH_1="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/Gpu-Hacks.patch"
 PATCH_2="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/KGSL-hacks-whitebelyash.diff"
 PATCH_3="https://github.com/lfdevs/mesa-for-android-container/commit/0a60c9c4108200fda20016b594dcf8806f29a28e.diff"
@@ -138,9 +138,7 @@ meson setup build-android-aarch64 \
     -Dvulkan-drivers=freedreno \
     -Dvulkan-beta=true \
     -Dfreedreno-kmds=kgsl \
-    -Degl=disabled \
-    -Dandroid-strict=false \
-    -Dshader-cache=enabled
+    -Degl=disabled
 
 ninja -C build-android-aarch64 install
 
